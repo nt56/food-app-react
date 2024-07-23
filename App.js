37abdf81ecc5ac45bb77aa038ee4a5bdfcdb33ea
nt_56 <nagutirth2121@gmail.com>
2024-07-23 12:07:58 +0530

    episode-3

--- App.js
+++ App.js
@@ -1,19 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", {id:"parent"}, 
-    [
-        React.createElement("div", {id:"child-1"},[
-            React.createElement("h1", {id:"heading"}, "I am  tag - 1"), 
-            React.createElement("h1", {id:"heading"}, "I am  tag - 2")
-        ]),
-        React.createElement("div", {id:"child-2"},[
-            React.createElement("h1", {id:"heading"}, "I am  tag - 3"), 
-            React.createElement("h1", {id:"heading"}, "I am  tag - 4")
-        ])
-    ]
-);
+const HeadingComponent = () => {
+    return (
+        <div>
+            <h1>Hello World From React Component</h1>
+        </div>
+    );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(<HeadingComponent />);
