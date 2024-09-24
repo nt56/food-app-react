6613a549befd4aedad59f4d52afdc9d907ec7619
nt_56 <nagutirth2121@gmail.com>
2024-09-24 17:12:22 +0530

    cors error resolved

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,10 +5,10 @@ export const LOGO_URL =
   "https://static.vecteezy.com/system/resources/previews/011/468/885/non_2x/food-logo-spoon-fork-icon-illustration-symbol-for-fast-delivery-app-restaurant-template-free-vector.jpg";
 
 export const MENU_API =
-  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.61610&lng=73.72860&restaurantId=";
+  "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.61610&lng=73.72860&restaurantId=";
 
 export const API =
-  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.61610&lng=73.72860&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+  "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.61610&lng=73.72860&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
 
 export const MY_PHOTO =
   "https://avatars.githubusercontent.com/u/113358781?s=400&u=7ac4ec17d09858d1eda2f27cb3a4e09f017d5b45&v=4";
